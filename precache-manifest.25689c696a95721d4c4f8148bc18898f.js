49e4b3575e256842a297a64e413cfb72e0449491
Octavio Ibanez <octaviogd@live.com.mx>
2020-09-30 19:56:04 -0500

    Updates

---
+++ precache-manifest.25689c696a95721d4c4f8148bc18898f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f876ab1a61c495b8a56119169e05e9",
+    "url": "/0ctavioibanez.github.io/catalogo/index.html"
+  },
+  {
+    "revision": "e2251f19e491c584fc10",
+    "url": "/0ctavioibanez.github.io/catalogo/static/css/main.011ca4a0.chunk.css"
+  },
+  {
+    "revision": "43558d77cb4e61d8fb0d",
+    "url": "/0ctavioibanez.github.io/catalogo/static/js/2.4851f895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/0ctavioibanez.github.io/catalogo/static/js/2.4851f895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2251f19e491c584fc10",
+    "url": "/0ctavioibanez.github.io/catalogo/static/js/main.140bd5da.chunk.js"
+  },
+  {
+    "revision": "8ec77283b75ece18e184",
+    "url": "/0ctavioibanez.github.io/catalogo/static/js/runtime-main.956fd279.js"
+  },
+  {
+    "revision": "5de519670ce62049a1c975dacbbbb757",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/aet300.5de51967.jpg"
+  },
+  {
+    "revision": "eb6a3a687a98b2240343c4619e39ebae",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/clh236.eb6a3a68.jpg"
+  },
+  {
+    "revision": "b3102aa6ae6073ed1c6642e91d4e7b37",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/cover.b3102aa6.jpg"
+  },
+  {
+    "revision": "4fb45c3c62af9b2f9b3bb1a5c3e50074",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/cr295.4fb45c3c.jpg"
+  },
+  {
+    "revision": "d69b7cf36b0caab807a9b4dc8175d7e1",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/e50-125.d69b7cf3.jpg"
+  },
+  {
+    "revision": "eea3bc95e4652560e2804e0b58c8fe25",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/e50-60.eea3bc95.jpg"
+  },
+  {
+    "revision": "cab3e6445f7119f79f5b1aea3121eb5a",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/eaa400.cab3e644.jpg"
+  },
+  {
+    "revision": "bf45eb64287564b05a2c0103c8fc0ec1",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/kfv290.bf45eb64.jpg"
+  },
+  {
+    "revision": "f0c7e1506c45bee62454fefbb9e5cce4",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/lrpa300.f0c7e150.jpg"
+  },
+  {
+    "revision": "0b79df11be990f5cd4958094deb2e8ea",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/mphp2.0b79df11.jpg"
+  },
+  {
+    "revision": "1b09e2ec82c4effc296711fe0e2cafe2",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/nhbm85.1b09e2ec.jpg"
+  },
+  {
+    "revision": "a57aa9cbe7815880672402234de4b485",
+    "url": "/0ctavioibanez.github.io/catalogo/static/media/nhc400.a57aa9cb.jpg"
+  }
+]);
